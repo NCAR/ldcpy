7fd0f1f10d088efebe0f61f46425039c3e99efb4
Alex <pinard.alex@gmail.com>
2020-06-17 11:40:02 -0600

    fixing test_plot data directories

--- tests/test_plot.py
+++ tests/test_plot.py
@@ -8,21 +8,21 @@ from ldcpy.error_metrics import ErrorMetrics
 
 ds = ldcpy.open_datasets(
     [
-        '../data/cam-fv/orig.TS.100days.nc',
-        '../data/cam-fv/zfp1.0.TS.100days.nc',
-        '../data/cam-fv/zfp1e-1.TS.100days.nc',
+        'data/cam-fv/orig.TS.100days.nc',
+        'data/cam-fv/zfp1.0.TS.100days.nc',
+        'data/cam-fv/zfp1e-1.TS.100days.nc',
     ],
     ['orig', 'recon', 'recon2'],
 )
 ds2 = ldcpy.open_datasets(
     [
-        '../data/cam-fv/orig.PRECT.100days.nc',
-        '../data/cam-fv/zfp1e-7.PRECT.100days.nc',
-        '../data/cam-fv/zfp1e-11.PRECT.100days.nc',
+        'data/cam-fv/orig.PRECT.100days.nc',
+        'data/cam-fv/zfp1e-7.PRECT.100days.nc',
+        'data/cam-fv/zfp1e-11.PRECT.100days.nc',
     ],
     ['orig', 'recon', 'recon_2'],
 )
-ds3 = ldcpy.open_datasets(['../data/cam-fv/cam-fv.T.6months.c'], ['orig'])
+ds3 = ldcpy.open_datasets(['data/cam-fv/cam-fv.T.6months.c'], ['orig'])
 
 
 class TestPlot(TestCase):
