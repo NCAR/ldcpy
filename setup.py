6890157f411a4eb589a2fecddc862175131e51c0
Alex <pinard.alex@gmail.com>
2020-07-09 22:15:33 -0700

    version 0.2 tag update

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from os.path import exists
 from setuptools import find_packages, setup
 from setuptools.command.install import install
 
-VERSION = 'v0.1'
+VERSION = 'v0.2'
 
 with open('requirements.txt') as f:
     install_requires = f.read().strip().split('\n')
