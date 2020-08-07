d91681c95f77fd89d7d60cacf4755fbb75ea8f2a
Allison Baker <abaker@ucar.edu>
2020-08-07 16:34:45 -0600

    Added utility to check metrics for pass/fail

--- ldcpy/__init__.py
+++ ldcpy/__init__.py
@@ -1,3 +1,3 @@
 from .metrics import DatasetMetrics, DiffMetrics
 from .plot import plot
-from .util import open_datasets, print_stats, collect_datasets
+from .util import open_datasets, print_stats, collect_datasets, check_metrics
