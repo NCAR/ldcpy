5e599cdb4683611b74d5052fd6994bbf68e30fc6
Alex <pinard.alex@gmail.com>
2020-08-17 20:04:35 -0600

    update mock imports

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -33,7 +33,7 @@ release = '0.1'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = ['nbsphinx', 'sphinx.ext.autodoc', 'sphinx.ext.napoleon']
-autodoc_mock_imports = ['cartopy', 'tlz', 'cmocean', 'xarray', 'xrft', 'scipy']
+autodoc_mock_imports = ['cartopy', 'tlz', 'cmocean', 'xarray', 'xrft', 'scipy', 'opencv-python']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
