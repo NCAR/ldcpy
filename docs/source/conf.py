98d19619d5f77ba4cef5d1482eb3171f67bafc9d
Alex <pinard.alex@gmail.com>
2020-08-21 12:59:04 -0600

    disable execution for unexecuted notebooks

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -32,6 +32,7 @@ release = '0.1'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
+nbsphinx_execute = 'never'
 extensions = ['nbsphinx', 'sphinx.ext.autodoc', 'sphinx.ext.napoleon']
 autodoc_mock_imports = [
     'cartopy',
