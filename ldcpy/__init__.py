c27efc5d20309ddbb11c52d8e24b61f444a843af
Anderson Banihirwe <axbanihirwe@ualr.edu>
2020-11-12 00:35:16 -0700

    Use setuptools_scm for versioning

--- ldcpy/__init__.py
+++ ldcpy/__init__.py
@@ -1,3 +1,14 @@
+#!/usr/bin/env python3
+# flake8: noqa
+""" Top-level module for ldcpy. """
+from pkg_resources import DistributionNotFound, get_distribution
+
+from .dataarray_metrics import MetricsAccessor
 from .metrics import DatasetMetrics, DiffMetrics
 from .plot import plot
 from .util import check_metrics, collect_datasets, compare_stats, open_datasets
+
+try:
+    __version__ = get_distribution(__name__).version
+except DistributionNotFound:  # pragma: no cover
+    __version__ = '0.0.0'  # pragma: no cover
